f57759e0915a72073c4755e30a8f14cb7840e710
ashiparua <ashiparua@gmail.com>
2025-08-25 00:05:12 +0530

    Delete maximum number .py

--- maximum number .py
+++
@@ -1,9 +0,0 @@
-a = int (input("enter the no."))
-b= int (input("enter the no."))
-c= int (input("enter the no."))
-if (a<b and a<c):
-    print("a is max")
-elif(b<c and b<a):
-    print("b is max")
-else:
-    print("c is max")
