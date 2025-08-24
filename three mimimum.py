0e9d47c5de9f0dd8c6126ad4a490d7b282e7886f
ashiparua <ashiparua@gmail.com>
2025-08-25 00:03:45 +0530

    Delete three mimimum.py

--- three mimimum.py
+++
@@ -1,9 +0,0 @@
-a = int (input("enter the no."))
-b = int(input("enter the  no."))
-c= int(input("enter the  no."))
-if (a<b and a<c):
-    print("a is min")
-elif(b<c and b<a):
-    print("b is min")
-else:
-    print("c is min")
